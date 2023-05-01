2fb3a9366b9baae331bbd748ddb4b545111d3cf7
Matthew Russell <mattrussell36@gmail.com>
2023-05-01 11:22:39 -0700

    add market context

--- src/trade.rs
+++ src/trade.rs
@@ -10,18 +10,23 @@ pub struct Props {
 pub fn Trade(props: &Props) -> Html {
     let tradable_instrument = props.market.tradable_instrument.clone();
     let instrument = tradable_instrument.instrument.clone();
+
     html! { 
-        <div>
-            <div class="p-4 border-b border-slate-200">
-                <h1 class="text-lg">{instrument.code}</h1> 
-                <p class="text-sm text-slate-600 mb-1">{instrument.name}</p>
-                <div class="flex gap-1">
-                    <Pill>{"Status: "}{props.market.state.clone()}</Pill> 
-                    <Pill>{"Trading mode: "}{props.market.trading_mode.clone()}</Pill> 
+        <ContextProvider<Market> context={props.market.clone()}>
+            <div>
+                <div class="p-4 border-b border-slate-200">
+                    <h1 class="text-lg">{instrument.code}</h1> 
+                    <p class="text-sm text-slate-600 mb-1">{instrument.name}</p>
+                    <div class="flex gap-1">
+                        <Pill>{"Status: "}{props.market.state.clone()}</Pill> 
+                        <Pill>{"Trading mode: "}{props.market.trading_mode.clone()}</Pill> 
+                    </div>
+                </div>
+                <div class="p-4">
+                    <Info /> 
                 </div>
             </div>
-            <div class="p-4">{"Content"}</div>
-        </div>
+        </ContextProvider<Market>>
     }
 }
 
@@ -36,5 +41,28 @@ pub fn Pill(props: &PillProps) -> Html {
         <div class="bg-slate-300 rounded px-1 py-0.5 text-xs">
             { for props.children.iter() }
         </div>
+    }
+}
+
+#[function_component]
+pub fn Info() -> Html {
+    let market = use_market();
+
+    html! { 
+        <div class="bg-slate-300 rounded py-2 px-4">
+            <h1>{market.tradable_instrument.instrument.code}</h1>
+        </div>
+    }
+}
+
+#[hook]
+fn use_market() -> Market {
+    let market = use_context::<Market>();
+
+    match market {
+        Some(data) => data,
+        None => {
+            panic!("use_market must be used inside a market context");
+        }
     }
 }
